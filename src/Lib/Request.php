5eb5e41218283379e99aa45ccd7919d68082f6be
Fábio Dias <fabio@studiogt.com.br>
2024-10-13 18:08:40 -0300

    Fix base uri

--- src/Lib/Request.php
+++ src/Lib/Request.php
@@ -12,10 +12,10 @@ class Request extends HttpRequest
         if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] != '') {
             $_SERVER['REQUEST_URI'] = rtrim($_SERVER['REQUEST_URI'], '/');
         }
-        
+
         $base = rtrim(dirname(dirname($_SERVER['SCRIPT_NAME'])), '/') . '/';
         $_SERVER['REQUEST_URI'] = str_replace(rtrim($base, '/'), '', $_SERVER['REQUEST_URI']);
-        
+
         $request =  parent::createFromGlobals();
         try {
             if ($request->getContentTypeFormat() == 'json') {
@@ -23,8 +23,7 @@ class Request extends HttpRequest
                 $request->request = $data;
             }
         } catch (Throwable $e) {
-
-        }  
+        }
 
 
         return $request;
